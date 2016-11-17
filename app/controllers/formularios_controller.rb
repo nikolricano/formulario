0e3d5407a774761a8496e681ed16fe603d8fb706
Oscar Hugo Cardenas Lopez <shanoorsucrel@gmail.com>
2016-11-16 21:05:54 -0600

    logic to not save if blank

--- app/controllers/formularios_controller.rb
+++ app/controllers/formularios_controller.rb
@@ -5,7 +5,7 @@ class FormulariosController < ApplicationController
   end
 
   def formularios_post
-    if params[:user][:name] == '' || params[:user][:age] == '' || params[:user][:address] == ''
+    if params[:user][:name].blank? || params[:user][:age].blank? || params[:user][:address].blank?
       redirect_to formularios_formularios_path, alert: 'No pude guardar me falta algo'
     else
       @user = User.create(name: params[:user][:name], age: params[:user][:age], address: params[:user][:address])
